eace55a03db7a96271c17672b4a80f72617577f3
Soumya02sen <102529346+Soumya02sen@users.noreply.github.com>
2024-08-30 01:32:28 +0530

    First commit

--- AgeOfWar.java
+++ AgeOfWar.java
@@ -1,1 +1,24 @@
+import java.util.*;
 
+public class AgeOfWar {
+
+    static class Platoon {
+        String type;
+        int soldiers;
+
+        Platoon(String type, int soldiers) {
+            this.type = type;
+            this.soldiers = soldiers;
+        }
+}
+
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Enter platoons
+        System.out.println("Enter your platoons:");
+        String ownInput = scanner.nextLine();
+        System.out.println("Enter opponent's platoons:");
+        String opponentInput = scanner.nextLine();
+}
+}
